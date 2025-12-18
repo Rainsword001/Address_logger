3b99798aef51bab50939962ce6e66fb340772f7f
Rainsword001 <olowoyoemmanuel12@gmail.com>
2025-12-18 11:58:13 +0000

    import path in app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ import { globalErrorHandler } from "./middlewares/error.middleware.js";
 
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
-const swaggerDocument = YAML.load("C:/Users/hp/Desktop/Address Logger updated/config/swagger.yaml");
+import path from 'path';
+const swaggerDocument = YAML.load(path.resolve('config/swagger.yaml'));
 
 const app = express();
 
